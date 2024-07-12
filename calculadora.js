8b62069c10d73a88010d28e6d312c8ef2466cf6f
MeminJR03 <153556162+MeminJR03@users.noreply.github.com>
2024-07-12 17:15:25 -0500

    calculadora personal
    .....

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+//Deivid Stiven Achury Sandoval CC:1031145748
+// var btnsuma = document.getElementById("btnSUMA")
+
+// var result = document.getElementById("txtRESUL")
+
+// btnsuma.addEventListener("click", sumar)
+
+function agregardato(numero)
+{
+    var resultado = document.getElementById('resultado');
+    resultado.value += numero;
+}
